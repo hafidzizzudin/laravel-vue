e338b79d57e30cab1d7adb666231b088a621dcdd
hafidz.izzudin <hafidz.izzudin49@gmail.com>
2024-02-01 22:22:13 +0700

    [SPA] reactive page but refresh when change route

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,10 +1,22 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
-    <!-- Persistent Layout to make a template of layouting -->
+    <div>The page with time {{ timer }}</div>
+    <!-- Layout to make a template of layouting -->
     <slot>Default</slot>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import { ref } from "vue";
+
+// reactive variable ref
+const timer = ref(0);
+
+setInterval(() => {
+    timer.value++;
+    if (timer.value % 10 === 0) {
+        timer.value = 0;
+    }
+}, 1000);
 </script>
