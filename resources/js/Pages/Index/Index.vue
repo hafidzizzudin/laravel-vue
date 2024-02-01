85bd13440aaa139973417112d710223516aaba81
hafidz.izzudin <hafidz.izzudin49@gmail.com>
2024-02-01 21:56:17 +0700

    [SPA] passing data from controller to vue

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,8 +1,17 @@
 <template>
     <div>Index</div>
     <Link href="/hello">Show Page</Link>
+    <!-- interpolation syntax or master syntax {{  }} -->
+    <div>The message is {{ message }}</div>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
+// prop declaration https://vuejs.org/guide/components/props.html
+defineProps({
+    message: {
+        type: String,
+        required: true,
+    },
+});
 </script>
